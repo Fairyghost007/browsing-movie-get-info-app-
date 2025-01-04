b419cef67a7eb9236f00d2a6dc02eeac25282a5d
Fairyghost007 <biranenini6762@gmail.com>
2025-01-04 18:17:24 +0000

    chore:added favicon

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="w-full mx-auto p-4 h-full bg-darkPurple m-0">
       <h1 className="text-3xl font-bold text-center mb-6 bg-lightPurple p-4 rounded-lg text-darkRose w-full">
-        Movie List App
+        Movie App
       </h1>
       {location.pathname === '/' && (
         <div className="flex flex-row items-baseline justify-between gap-2 h-auto">
@@ -50,7 +50,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<MovieList movies={filteredMovies} />} />
-
         <Route path="/movies/:title" element={<MovieDetails movies={movies} />}/>
       </Routes>
     </div>
